2df5cd050918d1a117534543c76a41d8d174f229
IssmailNAIM <42590789+IssmailNAIM@users.noreply.github.com>
2021-07-05 03:21:54 +0000

    Update VerificationController.php

--- app/Http/Controllers/auth/VerificationController.php
+++ app/Http/Controllers/auth/VerificationController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class VerificationController extends Controller
 {
